0caf79228045468bd1bd6c998c54bbad16a3a041
SonchikSonch <90112310+SonchikSonch@users.noreply.github.com>
2021-09-23 23:24:12 +0300

    Add files via upload

---
+++ c11.py
@@ -0,0 +1,10 @@
+a=int(input())
+b=int(input())
+if a>b:
+    print (a)
+elif b>a:
+    print(b)
+elif a==b:
+    print('равные')
+    
+    
---
+++ c12.py
@@ -0,0 +1,12 @@
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+e = float(input())
+x = (a*b-c)
+z = (e-d)
+if z == 0:
+     print('на 0 делить нельзя')
+else:
+     print(x/z)
+ 
