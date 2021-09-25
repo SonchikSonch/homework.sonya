7f194ad762253be4abf66fe9a9508f69dba4c76f
SonchikSonch <90112310+SonchikSonch@users.noreply.github.com>
2021-09-25 09:12:11 +0300

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,9 @@
+from math import sqrt
+a=float(input('a =  '))
+b=float(input('b =  '))
+c=float(input('c =  '))
+x = sqrt (a ** 2 + b ** 2 + c ** 2)
+print(x)
+
+
+    
---
+++ 15.py
@@ -0,0 +1,14 @@
+x=float(input())
+y=float(input())
+if x > 0 and y > 0:
+    print('1 четверть')
+elif x < 0 and y > 0:
+    print('2 четверть')
+elif x < 0 and y < 0:
+    print('3 четверть')    
+elif x > 0 and y < 0:
+    print('4 четверть')    
+else x == 0 and y == 0:
+    print('начало координат')    
+    
+    
