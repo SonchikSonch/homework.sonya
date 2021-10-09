8052ae003db591634ed5153e09296cfd2bc4a6f0
SonchikSonch <90112310+SonchikSonch@users.noreply.github.com>
2021-10-09 08:51:48 +0300

    Add files via upload

---
+++ 1.2.py
@@ -0,0 +1,11 @@
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+e = float(input())
+x = a * b - c
+z = e - d
+if z == 0:
+     print('на 0 делить нельзя')
+else:
+     print(x / y)
---
+++ 1.3.py
@@ -0,0 +1,6 @@
+from math import sqrt
+a = float(input('a =  '))
+b = float(input('b =  '))
+c = float(input('c =  '))
+x = sqrt(a ** 2 + b ** 2 + c ** 2)
+print(x)
